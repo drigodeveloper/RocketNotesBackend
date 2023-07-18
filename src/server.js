1c63ba8950dda9bf7c4b093cb0064a50d55d63c5
Rodrigo <drigodeveloper@gmail.com>
2023-07-17 21:30:37 -0300

    Adicionando o nodemon

--- src/server.js
+++ src/server.js
@@ -2,6 +2,19 @@ const express = require("express");
 
 const app = express();
 
-const PORT = 3333;
+app.get("/message/:id/:user", (request, response) => {
+
+    const { id, user } = request.params
+
+    response.send(`Id da mensagem: ${id}. 
+    Para o usuário: ${user}`)
+})
+
+app.get("/users", (request, response) => {
+    const { page, limit } = request.query;
+    response.send(`Página: ${page} Mostrar ${limit}`)
+})
+
+const PORT = 3332;
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
