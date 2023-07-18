5349511c256f5ee932bcc3aef04dd12340aa1397
Rodrigo <drigodeveloper@gmail.com>
2023-07-17 22:28:11 -0300

    Criando o metodo posto e mudando o formato de resposta para JSON

--- src/server.js
+++ src/server.js
@@ -1,18 +1,13 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
-app.get("/message/:id/:user", (request, response) => {
-
-    const { id, user } = request.params
-
-    response.send(`Id da mensagem: ${id}. 
-    Para o usuário: ${user}`)
-})
-
-app.get("/users", (request, response) => {
-    const { page, limit } = request.query;
-    response.send(`Página: ${page} Mostrar ${limit}`)
+app.post("/users", (request, response) => {
+    
+    const { name, email, password } = request.body;
+    
+    response.json( {name, email, password} );
 })
 
 const PORT = 3332;
