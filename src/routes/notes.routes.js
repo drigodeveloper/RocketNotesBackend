78c8603a19a48131fc94ebbb97354cc153f2d175
Rodrigo <drigodeveloper@gmail.com>
2023-07-29 20:20:54 -0300

    Criando a rota para exibir as notas

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -1,12 +1,13 @@
 const { Router } = require("express");
 
-const NotesConrtoller = require("../controllers/NotesConrtoller")
+const NotesController = require("../controllers/NotesController")
 
 const notesRoutes = Router();
 
-const notesConrtoller = new NotesConrtoller()
+const notesConrtoller = new NotesController();
 
 
 notesRoutes.post("/:user_id", notesConrtoller.create);
+notesRoutes.get("/:id", notesConrtoller.show);
 
 module.exports = notesRoutes;
