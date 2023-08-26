cbee4df1cf530bc500449a4413941189c1774f46
Rodrigo <drigodeveloper@gmail.com>
2023-08-26 20:12:54 -0300

    Adicionando o JWT

--- src/controllers/sessionsController.js
+++ src/controllers/sessionsController.js
@@ -1,9 +1,28 @@
+const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
+const { Compare, compare } = require("bcryptjs");
+
 class SessionsController {
-    async create(response, request) {
+    async create(request, response) {
         const { email, password } = request.body
 
-        return response.json({ email, password });
+        const user = await knex("users").where({ email }).first();
+
+        if(!user) {
+            throw new AppError("Email e ou senha incorretas", 401)
+        }
+
+        const passwordChecked = await compare(password, user.password);
+
+        if(!passwordChecked) {
+            throw new AppError("E mail e ou senha incorreta", 401);
+        }
+
+        return response.json(user);
     }
 };
 
 module.exports = SessionsController;
+
+
+// { email, password }
