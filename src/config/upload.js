3993059a44ab460e6e276bf46887364f1b531d52
Rodrigo <drigodeveloper@gmail.com>
2023-09-07 15:31:40 -0300

    Error na importação do multer

--- src/config/upload.js
+++ src/config/upload.js
@@ -1,4 +1,26 @@
-const path = require("path")
+const path = require("path");
+const multer = require("../../.vscode/settings.json");
+const crypto = require("crypto");
 
-const TMP_FOLDER = path.resolve("..", "..", "tmp");
+const TMP_FOLDER = path.resolve(__dirname, "..", "..", "tmp");
+const UPLOAD_FOLDER = path.resolve(__dirname, "uploads");
 
+
+const MULTER = { 
+    storage: multer.diskStorage({
+        destination: TMP_FOLDER,
+        filename(request, file, callback) {
+            const fileHash = crypto.randomBytes(10).toString("hex"); //função criada usando crypto para gerar um hash, para dar um nome para o arquivo, é usado o hash para evitar que dois arquivos tenham nomes iguais 
+            const filaName = `${fileHash}-${file.originalname}`;
+
+            return callback(null, filaName);
+        }
+
+    })
+};
+
+module.exports = {
+    TMP_FOLDER,
+    UPLOAD_FOLDER,
+    MULTER
+};
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,13 +1,21 @@
-const { Router } = require("express");
+const { Router, response } = require("express");
+const path = require("path");
+const multer = require("multer");
+const uploadConfig = require("../config/upload");
 
-const UsersController = require("../controllers/UsersController")
-const ensureAuthentication = require("../middlewares/ensureAuthentication")
+const UsersController = require("../controllers/UsersController");
+const ensureAuthentication = require("../middlewares/ensureAuthentication");
 
 const usersRoutes = Router();
+const upload = multer(uploadConfig.MULTER)
 
 const usersController = new UsersController();
 
 usersRoutes.post("/", usersController.create);
 usersRoutes.put("/", ensureAuthentication, usersController.update);
+usersRoutes.patch("/avatar", ensureAuthentication, upload.single("avatar"), (request, response) => {
+    console.log(request.file.fillename)
+    return response.json();
+});
 
 module.exports = usersRoutes;
