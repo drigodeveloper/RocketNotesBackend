41c7f7744154fe4072103be3996ee7fabc5c7ec3
Rodrigo <drigodeveloper@gmail.com>
2023-10-18 23:07:55 -0300

    criando a pasta de uploads

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,18 +4,18 @@ const multer = require("multer");
 const uploadConfig = require("../config/upload");
 
 const UsersController = require("../controllers/UsersController");
+const UserAvatarController = require("../controllers/UserAvatarController");
 const ensureAuthentication = require("../middlewares/ensureAuthentication");
 
 const usersRoutes = Router();
 const upload = multer(uploadConfig.MULTER)
 
 const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
+
 
 usersRoutes.post("/", usersController.create);
 usersRoutes.put("/", ensureAuthentication, usersController.update);
-usersRoutes.patch("/avatar", ensureAuthentication, upload.single("avatar"), (request, response) => {
-    console.log(request.file.fillename)
-    return response.json();
-});
+usersRoutes.patch("/avatar", ensureAuthentication, upload.single("avatar"), userAvatarController.update);
 
 module.exports = usersRoutes;
